692ac9716e689bbf75a369379e49d8bc56fa9112
Stephen Seltzer <sgs116@psu.edu>
2016-08-29 17:15:41 -0400

    Adds car test inheritance classes.

---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/interfaces/BodyTests.java
@@ -0,0 +1,5 @@
+package edu.psu.swe.testing.junit5.presentation.interfaces;
+
+public class BodyTests {
+
+}
---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/interfaces/EngineTests.java
@@ -0,0 +1,60 @@
+package edu.psu.swe.testing.junit5.presentation.interfaces;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+public interface EngineTests<T> extends CarTests<T> {
+
+	
+	T createEngineTests();
+	abstract boolean isEngineRunning();
+	abstract void startEngine();
+	abstract void stopEngine();
+	abstract int getRPM();
+	abstract int addFuel(int percentIncrease);
+	abstract int reduceFuel(int percentDecrease);
+	abstract void hitKillSwitch();
+	 
+	
+	@BeforeEach
+	default void startEngineTest(){
+		startEngine();
+		assertTrue(isEngineRunning(), "Engine failed to start.");
+	}
+	
+	@AfterEach
+	default void stopEngineTest(){
+		stopEngine();
+		assertFalse(isEngineRunning(), "Engine failed to stop. Current RPM is " + getRPM() + ".");
+	}
+	
+	@Test
+	default void addFuelTest(){
+		int idleRpm = getRPM();
+		addFuel(100);
+		assertTrue(getRPM() > idleRpm, "Check RPMs with 100% fuel increase. Current RPM is " + getRPM());	
+	}
+	
+	@Test
+	default void removeFuelTest(){
+		int idleRpm = getRPM();
+		reduceFuel(50);
+		assertTrue(getRPM() < idleRpm, "Check RPMs with 50% fuel decrease. Current RPM is " + getRPM());	
+	}
+
+	@Test
+	//Is this where the conditional disabling goes?
+	default void killSwitchTest(){
+		assertTrue(isEngineRunning(), "Engine not running.");
+		hitKillSwitch();
+		assertFalse(isEngineRunning(), "Engine should had stopped with killswitch. Current RPM is " + getRPM());
+	}
+}
