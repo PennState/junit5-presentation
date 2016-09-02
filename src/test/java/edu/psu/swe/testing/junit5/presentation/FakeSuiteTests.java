8e275d67e68878728565b5ac496303797ae221f8
Steve Moyer <smoyer1@selesy.com>
2016-09-02 16:19:33 -0400

    Added a fake suite so we could simulate @BeforeSuite and @AfterSuite.

---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/FakeSuiteTests.java
@@ -0,0 +1,25 @@
+package edu.psu.swe.testing.junit5.presentation;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+
+public class FakeSuiteTests {
+  
+  @BeforeAll
+  public static void beforeSuite() {
+    System.out.println("Before suite");
+  }
+  
+  @AfterAll
+  public static void afterSuite() {
+    System.out.println("After suite");
+  }
+  
+  @Nested
+  public class FirstTests extends SuiteTestsOne {}
+  
+  @Nested
+  public class SecondTests extends SuiteTestsTwo {}
+
+}
---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/SuiteTestsTwo.java
@@ -0,0 +1,45 @@
+package edu.psu.swe.testing.junit5.presentation;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public abstract class SuiteTestsTwo {
+
+  @BeforeAll
+  public static void beforeAll() {
+    System.out.println("Before all two");
+  }
+  
+  @AfterAll
+  public static void afterAll() {
+    System.out.println("After all two");
+  }
+  
+  @BeforeEach
+  public void beforeEach() {
+    System.out.println("Before each two");
+  }
+  
+  @AfterEach
+  public void afterEach() {
+    System.out.println("After each two");
+  }
+  
+  @Test
+  public void testTwoOne() {
+    System.out.println("test two one");
+    assertTrue(true);
+  }
+  
+  @Test
+  public void testTwoTwo() {
+    System.out.println("test two two");
+    assertTrue(true);
+  }
+  
+}
