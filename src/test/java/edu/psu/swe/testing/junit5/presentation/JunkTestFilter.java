de32c792472d5221530df4aba7efa6be3f87bc2f
Steve Moyer <smoyer1@selesy.com>
2016-09-06 17:01:33 -0400

    Added an example of how conditional test execution can be done.

---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/JunkTestFilter.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package edu.psu.swe.testing.junit5.presentation;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.TestExecutionCondition;
+import org.junit.jupiter.api.extension.TestExtensionContext;
+
+/**
+ * @author smoyer1
+ *
+ */
+public class JunkTestFilter implements TestExecutionCondition {
+
+  /* (non-Javadoc)
+   * @see org.junit.jupiter.api.extension.TestExecutionCondition#evaluate(org.junit.jupiter.api.extension.TestExtensionContext)
+   */
+  @Override
+  public ConditionEvaluationResult evaluate(TestExtensionContext context) {
+    ConditionEvaluationResult result = ConditionEvaluationResult.enabled("This test is not junky");
+    
+    if(context.getTestMethod().isPresent()) {
+      Method method = context.getTestMethod().get();
+      if(method.getName().toLowerCase().contains("junk")) {
+        result = ConditionEvaluationResult.disabled("This test is junky");
+      }
+    }
+    
+    return result;
+  }
+
+}
---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/NewCarTests.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package edu.psu.swe.testing.junit5.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+/**
+ * @author smoyer1
+ *
+ */
+@ExtendWith(JunkTestFilter.class)
+public class NewCarTests {
+  
+  @Test
+  public void testWhetherSaabsAreNice() {
+    System.out.println("testWhetherSaabsAreNice()");
+  }
+  
+  @Test
+  public void testWhetherYugosAreJunky() {
+    System.out.println("testWhetherYugosAreJunky()");
+  }
+
+}
