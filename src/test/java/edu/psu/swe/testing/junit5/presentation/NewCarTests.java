34a53e05971426674f395c0497c2e5d9de2f152e
Steve Moyer <smoyer1@selesy.com>
2016-09-07 13:52:12 -0400

    Added code to show how assumptions skip tests.

--- src/test/java/edu/psu/swe/testing/junit5/presentation/NewCarTests.java
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/NewCarTests.java
@@ -3,7 +3,12 @@
  */
 package edu.psu.swe.testing.junit5.presentation;
 
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
@@ -14,13 +19,32 @@ import org.junit.jupiter.api.extension.ExtendWith;
 public class NewCarTests {
   
   @Test
-  public void testWhetherSaabsAreNice() {
+  public void testWhetherSaabsAreNice(TestInfo testInfo) {
+    assumeFalse(testMethodNameContainsWeird(testInfo));
     System.out.println("testWhetherSaabsAreNice()");
   }
   
   @Test
-  public void testWhetherYugosAreJunky() {
+  public void testWhetherYugosAreJunky(TestInfo testInfo) {
+    assumeFalse(testMethodNameContainsWeird(testInfo));
     System.out.println("testWhetherYugosAreJunky()");
+  }
+  
+  @Test
+  public void testWhetherHermanMuenstersCarIsWeird(TestInfo testInfo) {
+    assumeFalse(testMethodNameContainsWeird(testInfo));
+    System.out.println("testWhetherHermanMuenstersCarIsWeird()");
+  }
+  
+  boolean testMethodNameContainsWeird(TestInfo testInfo) {
+    boolean weird = false;
+    
+    if(testInfo.getTestMethod().isPresent()) {
+      Method method = testInfo.getTestMethod().get();
+      weird = method.getName().toLowerCase().contains("weird");
+    }
+    
+    return weird;
   }
 
 }
