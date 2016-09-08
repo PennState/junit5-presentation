89bea9c9ef05a4a2dbcdcc5c26459ccf0b4ccd69
Niraja Ramesh <nur1@psu.edu>
2016-09-08 09:26:49 -0400

    adding Extension tests

--- src/test/java/edu/psu/swe/testing/junit5/presentation/tests/SimpleAssertionTests.java
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/tests/SimpleAssertionTests.java
@@ -2,10 +2,13 @@ package edu.psu.swe.testing.junit5.presentation.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 import edu.psu.swe.testing.junit5.presentation.affiliates.tests.SampleStudents;
 
 public class SimpleAssertionTests {
 	
+	@Test
 	void checkStudent() {
 		assertEquals("Builder", SampleStudents.getFullTimeStudent().getLastName());
 	}
