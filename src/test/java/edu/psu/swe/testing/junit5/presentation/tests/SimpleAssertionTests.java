405b81bb205a2ea7f2a8e73d2825c87ff79b8732
Niraja Ramesh <nur1@psu.edu>
2016-09-08 10:01:18 -0400

    updates simple tests

--- src/test/java/edu/psu/swe/testing/junit5/presentation/tests/SimpleAssertionTests.java
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/tests/SimpleAssertionTests.java
@@ -1,6 +1,7 @@
 package edu.psu.swe.testing.junit5.presentation.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,13 @@ import edu.psu.swe.testing.junit5.presentation.affiliates.tests.SampleStudents;
 public class SimpleAssertionTests {
 	
 	@Test
-	void checkStudent() {
+	void checkStudentLastName() {
 		assertEquals("Builder", SampleStudents.getFullTimeStudent().getLastName());
+	}
+	
+	@Test
+	void checkStudentFirstName() {
+		assertTrue("Bob".equals(SampleStudents.getFullTimeStudent().getFirstName()));
 	}
 	
 	
