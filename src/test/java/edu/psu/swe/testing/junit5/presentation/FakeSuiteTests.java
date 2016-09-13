1a1f324c3050c0579d23c1648f3400729911fc4b
Niraja Ramesh <nur1@psu.edu>
2016-09-13 16:29:18 -0400

    adding rest of the slides

--- src/test/java/edu/psu/swe/testing/junit5/presentation/FakeSuiteTests.java
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/FakeSuiteTests.java
@@ -2,11 +2,9 @@ package edu.psu.swe.testing.junit5.presentation;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@Disabled
 @ExtendWith(SuiteTiming.class)
 public class FakeSuiteTests {
   
