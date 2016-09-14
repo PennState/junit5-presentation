3030b447b7d4b3848ce618b22da41e13a3d93a84
Stephen Seltzer <sgs116@psu.edu>
2016-09-14 15:18:54 -0400

    Adds fakeSuiteTest classes

---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/tests/suite/FakeSuiteTests.java
@@ -0,0 +1,27 @@
+package edu.psu.swe.testing.junit5.presentation.tests.suite;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SuiteTiming.class)
+public class FakeSuiteTests {
+  
+  @BeforeAll
+  public static void beforeSuite() {
+    System.out.println("Before suite");
+  }
+  
+  @AfterAll
+  public static void afterSuite() {
+    System.out.println("After suite");
+  }
+  
+  @Nested
+  public class FirstTests extends SuiteTestsOne {}
+  
+  @Nested
+  public class SecondTests extends SuiteTestsTwo {}
+
+}
---
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/tests/suite/SuiteTestsOne.java
@@ -0,0 +1,47 @@
+package edu.psu.swe.testing.junit5.presentation.tests.suite;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public abstract class SuiteTestsOne {
+
+  @BeforeAll
+  public static void beforeAll() {
+    System.out.println("Before all one");
+  }
+  
+  @AfterAll
+  public static void afterAll() {
+    System.out.println("After all one");
+  }
+  
+  @BeforeEach
+  public void beforeEach() {
+    System.out.println("Before each one");
+  }
+  
+  @AfterEach
+  public void afterEach() {
+    System.out.println("After each one");
+  }
+  
+  @Test
+  public void testOneOne() {
+    System.out.println("test one one");
+    assertTrue(true);
+  }
+  
+  @Test
+  public void testOneTwo() {
+    System.out.println("test one two");
+    assertTrue(true);
+  }
+  
+}
