23eb579fb8a36dc17667b1370182cf0834092ec0
Steve Moyer <smoyer@psu.edu>
2017-09-11 13:58:10 -0400

    Updates condition and method names to match release API.

--- src/test/java/edu/psu/swe/testing/junit5/presentation/tests/extensions/AfflliateDetector.java
+++ src/test/java/edu/psu/swe/testing/junit5/presentation/tests/extensions/AfflliateDetector.java
@@ -6,23 +6,23 @@ package edu.psu.swe.testing.junit5.presentation.tests.extensions;
 import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
-import org.junit.jupiter.api.extension.TestExecutionCondition;
-import org.junit.jupiter.api.extension.TestExtensionContext;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class AfflliateDetector implements TestExecutionCondition {
+public class AfflliateDetector implements ExecutionCondition {
 
-	@Override
-	public ConditionEvaluationResult evaluate(TestExtensionContext context) {
-		ConditionEvaluationResult result = ConditionEvaluationResult.enabled("is an affiliate");
+  @Override
+  public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+    ConditionEvaluationResult result = ConditionEvaluationResult.enabled("is an affiliate");
 
-		if (context.getTestMethod().isPresent()) {
-			Method method = context.getTestMethod().get();
-			if (!method.getName().toLowerCase().contains("affiliate")) {
-				result = ConditionEvaluationResult.disabled("is not an affiliate");
-			}
-		}
+    if (context.getTestMethod().isPresent()) {
+      Method method = context.getTestMethod().get();
+      if (!method.getName().toLowerCase().contains("affiliate")) {
+        result = ConditionEvaluationResult.disabled("is not an affiliate");
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 
 }
